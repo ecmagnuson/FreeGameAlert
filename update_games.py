e5c4c0d7431107a0243bf4a5f97172ff32f8e353
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-10-10 22:13:06 -0500

    changed GST to CST time

--- update_games.py
+++ update_games.py
@@ -1,4 +1,5 @@
 from utils import get_sendfrom_email, get_sendto_email, get_apppassword
+from datetime import datetime
 import feedparser
 import smtplib
 
@@ -25,7 +26,20 @@ def is_same_game():
     return get_newest_file_game() == get_newest_RSS_game()
 
 def get_offer_deadline():
+    #the description is formated the same way every time
     return d.entries[0].description[d.entries[0].description.index('Offer') : d.entries[0].description.index('GMT') + 3]
+
+def get_CST_deadline():
+    #convert GMT time to CST because I'm not british
+    #This is so convoluted, it's gonna break I feel
+    deadline = get_offer_deadline()
+    GMT = int(deadline[deadline.index(',') + 2 : deadline.index('GMT')])
+    CST_time = GMT - 500
+    CST_time = str(CST_time)
+    CST_time = CST_time[0 : 2] + ':' + CST_time[2:]
+    deadline = deadline[ : deadline.index(',') + 1]
+    d = datetime.strptime(CST_time, "%H:%M")
+    return f'{deadline} {d.strftime("%I:%M %p")}'
 
 def alert():
     #Google removed less-secure-apps now must use app password
@@ -36,9 +50,10 @@ def alert():
         email_password = get_apppassword()
         connection.login(email_address, email_password )
         connection.sendmail(from_addr=email_address, to_addrs=get_sendto_email(), 
-        msg=f'subject:Free Game! {get_offer_deadline()} \n\n Looks like there is a free game :D \n\n {d.entries[0].title} is free right now. \n See the link: {d.entries[0].link} \n\n {get_offer_deadline()}.')
+        msg=f'subject:Free Game! {get_offer_deadline()} \n\n Looks like there is a free game :D \n\n {d.entries[0].title} is free right now. \n See the link: {d.entries[0].link} \n\n {get_CST_deadline()}.')
 
 if __name__ == '__main__':
     if not is_same_game():
         alert()
         update_game_list()
+    
