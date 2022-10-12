5913a61b6e08dc8fd369c61bc5f7212eadb3c892
Elliott Magnuson <ecmagnuson@wisc.edu>
2022-10-11 22:43:20 -0500

    added type hinting

--- src/update_games.py
+++ src/update_games.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import feedparser
 import smtplib
+
 from datetime import datetime
 from os.path import dirname, abspath
 from utils import get_sendfrom_email, get_sendto_email, get_apppassword, get_newest_file_game
@@ -7,25 +10,25 @@ from utils import get_sendfrom_email, get_sendto_email, get_apppassword, get_new
 #RSS feed of free games - https://steamcommunity.com/groups/freegamesfinders/announcements
 d = feedparser.parse('https://steamcommunity.com/groups/freegamesfinders/rss/')
 
-def update_game_list():
+def update_game_list() -> None:
     #File of all games in RSS feed
     with open(dirname(dirname(abspath(__file__))) + '/games.txt','w') as f:
         for entry in d.entries:
             f.write(f'{entry.title}\n') 
 
-def get_newest_RSS_game():
+def get_newest_RSS_game() -> str:
     #get newest game on RSS feed
     return f'{d.entries[0].title}'.strip()
 
-def is_same_game():
+def is_same_game() -> bool:
     #check if RSS feed and top game from file is the same
     return get_newest_file_game() == get_newest_RSS_game()
 
-def get_offer_deadline():
+def get_offer_deadline() -> str:
     #the description is formated the same way every time
     return d.entries[0].description[d.entries[0].description.index('Offer') : d.entries[0].description.index('GMT') + 3]
 
-def get_CST_deadline():
+def get_CST_deadline() -> str:
     #convert GMT time to CST because I'm not british
     #This is so convoluted, it's gonna break I feel
     deadline = get_offer_deadline()
@@ -37,7 +40,7 @@ def get_CST_deadline():
     d = datetime.strptime(CST_time, "%H:%M")
     return f'{deadline} {d.strftime("%I:%M %p")}'
 
-def email():
+def email() -> None:
     #Google removed less-secure-apps now must use app password
     #https://stackoverflow.com/questions/72480454/sending-email-with-python-google-disables-less-secure-apps
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as connection:  
@@ -47,7 +50,10 @@ def email():
         connection.sendmail(from_addr=email_address, to_addrs=get_sendto_email(), 
         msg=f'subject:Free Game! {get_CST_deadline()} \n\n Looks like there is a free game :D \n\n {d.entries[0].title} is free right now. \n See the link: {d.entries[0].link} \n\n {get_CST_deadline()}.')
 
-if __name__ == '__main__':
+def main() -> None:
     if not is_same_game():
         email()
         update_game_list()
+
+if __name__ == '__main__':
+    main()
--- src/utils.py
+++ src/utils.py
@@ -1,21 +1,22 @@
+from __future__ import annotations
 from os.path import dirname, abspath
 
-def get_sendfrom_email():
+def get_sendfrom_email() -> str:
     #get the email to send from
     with open(dirname(dirname(abspath(__file__))) + '/auth/emailfrom','r') as f:
         return f.readline().strip()
 
-def get_sendto_email():
+def get_sendto_email() -> str:
     #get the email to send to
     with open(dirname(dirname(abspath(__file__))) + '/auth/emailto','r') as f:
         return f.readline().strip()
 
-def get_apppassword():
+def get_apppassword() -> str:
     #https://support.google.com/accounts/answer/185833?hl=en
     with open(dirname(dirname(abspath(__file__))) + '/auth/app_password', 'r') as f:
         return f.readline().strip()
 
-def get_newest_file_game():
+def get_newest_file_game() -> str:
     #get newest game on top of file
     with open(dirname(dirname(abspath(__file__))) + '/games.txt', 'r') as f:
         return f.readline().strip()
